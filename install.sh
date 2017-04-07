c77568cbd270afaef755726f23f0fba3cdaeb5b7
Mikko Rantanen <jubjub@jubjubnest.net>
2017-04-07 16:31:22 +0000

    Remove debug output

--- install.sh
+++ install.sh
@@ -3,14 +3,10 @@
 : ${HACKBERRY_DIR:="$HOME/.hackberry"}
 : ${HACKBERRY_GIT:="https://github.com/Rantanen/hackberry"}
 
-echo $HACKBERRY_DIR
-
 if cd $HACKBERRY_DIR
 then
-	pwd
 	git pull
 else
-	pwd
 	git clone $HACKBERRY_GIT $HACKBERRY_DIR
 	cd $HACKBERRY_DIR
 fi
